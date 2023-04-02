499ef680c8fe739009dfd000adc886bf561573ad
Khadijah <khadijah.celestine@gmail.com>
2023-04-02 01:32:47 -0400

    removing vars out of main tf file

--- main.tf
+++ main.tf
@@ -12,23 +12,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-variable "TFC_AWS_PLAN_ROLE_ARN" {
-  type        = string
-  description = "TFC_AWS_PLAN_ROLE_ARN"
-}
-
-variable "TFC_AWS_APPLY_ROLE_ARN" {
-  type        = string
-  description = "TFC_AWS_APPLY_ROLE_ARN"
-}
-
-variable "TFC_AWS_RUN_ROLE_ARN" {
-  type        = string
-  description = "TFC_AWS_RUN_ROLE_ARN"
-}
-
-variable "TFC_AWS_PROVIDER_AUTH" {
-  default     = true
-  description = "TFC_AWS_PROVIDER_AUTH"
-}
