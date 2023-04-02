8ea7d615de759bd0c992871d611f2f716c521265
Khadijah <khadijah.celestine@gmail.com>
2023-04-02 02:00:39 -0400

    add bucket acl && cors config

--- main.tf
+++ main.tf
@@ -20,3 +20,18 @@ resource "aws_s3_bucket" "bucket-1" {
 data "aws_s3_bucket" "selected-bucket" {
   bucket = aws_s3_bucket.bucket-1.bucket
 }
+
+resource "aws_s3_bucket_acl" "bucket-acl" {
+  bucket = data.aws_s3_bucket.selected-bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "example" {
+  bucket = data.aws_s3_bucket.selected-bucket.bucket
+cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://www.${var.domain_name}"]
+    max_age_seconds = 3000
+  }
+}
