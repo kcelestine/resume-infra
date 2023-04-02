bf5620ed012db38e7a905eb28b3fd58b60b75551
Khadijah <khadijah.celestine@gmail.com>
2023-04-02 01:54:05 -0400

    lots of s3 code

--- main.tf
+++ main.tf
@@ -14,36 +14,46 @@ provider "aws" {
 }
 
 
-# S3 bucket for website.
-resource "aws_s3_bucket" "www_bucket" {
-  bucket = "www.${var.bucket_name}"
-  acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+resource "aws_s3_bucket" "s3_demo_bucket" {
+    bucket = var.bucket_name
 
-  cors_rule {
-    allowed_headers = ["Authorization", "Content-Length"]
-    allowed_methods = ["GET", "POST"]
-    allowed_origins = ["https://www.${var.domain_name}"]
-    max_age_seconds = 3000
-  }
-
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
-
-  tags = var.common_tags
+    tags = var.tags
 }
 
-# S3 bucket for redirecting non-www to www.
-resource "aws_s3_bucket" "root_bucket" {
-  bucket = var.bucket_name
-  acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+resource "aws_s3_bucket_website_configuration" "s3_demo_bucket" {
+    bucket = aws_s3_bucket.s3_demo_bucket.id
 
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
-  }
+    index_document {
+        suffix = "index.html"
+    }
 
-  tags = var.common_tags
+    error_document {
+        key = "error.html"
+    }
+}
+
+resource "aws_s3_bucket_acl" "s3_demo_bucket" {
+    bucket = aws_s3_bucket.s3_demo_bucket.id
+
+    acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "s3_demo_bucket" {
+    bucket = aws_s3_bucket.s3_demo_bucket.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid = "PublicReadGetObject"
+                Effect = "Allow"
+                Principal = "*"
+                Action = "s3:GetObject"
+                Resource = [
+                    aws_s3_bucket.s3_demo_bucket.arn,
+                    "${aws_s3_bucket.s3_bucket.arn}/*",
+                ]
+            },
+        ]
+    })
 }
