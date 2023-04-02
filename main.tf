7bd346da9ac1d6d0e4d21cefe0ba038d5964cbeb
Khadijah <khadijah.celestine@gmail.com>
2023-04-02 02:06:35 -0400

    add bucket policy and policy doc

--- main.tf
+++ main.tf
@@ -35,3 +35,24 @@ resource "aws_s3_bucket_cors_configuration" "example" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = data.aws_s3_bucket.selected-bucket.id
+  policy = data.aws_iam_policy_document.iam-policy-1.json
+}
+
+data "aws_iam_policy_document" "iam-policy-1" {
+  statement {
+    sid    = "AllowPublicRead"
+    effect = "Allow"
+resources = [
+      "arn:aws:s3:::www.${var.domain_name}",
+      "arn:aws:s3:::www.${var.domain_name}/*",
+    ]
+actions = ["S3:GetObject"]
+principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
