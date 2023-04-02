401090de376939c270cf8aa845fc64ee54284fdb
Khadijah <khadijah.celestine@gmail.com>
2023-04-02 02:07:30 -0400

    adding website configuration

--- main.tf
+++ main.tf
@@ -56,3 +56,13 @@ principals {
     }
   }
 }
+
+
+resource "aws_s3_bucket_website_configuration" "website-config" {
+  bucket = data.aws_s3_bucket.selected-bucket.bucket
+index_document {
+    suffix = "index.html"
+  }
+error_document {
+    key = "404.jpeg"
+  }
