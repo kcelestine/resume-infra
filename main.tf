d95767241e099df33e0a80b8a5c1b9c24558e7ca
Khadijah <khadijah.celestine@gmail.com>
2023-04-02 02:21:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_cors_configuration" "example" {
 }
 
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.selected-bucket.id
+  bucket = data.aws_s3_bucket.selected-bucket.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
 
