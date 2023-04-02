deb5bbfba59f97ac92893598f9d7f8fa50b5937e
Khadijah <khadijah.celestine@gmail.com>
2023-04-02 02:01:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_acl" "bucket-acl" {
 
 resource "aws_s3_bucket_cors_configuration" "example" {
   bucket = data.aws_s3_bucket.selected-bucket.bucket
-cors_rule {
+  cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
     allowed_methods = ["GET", "POST"]
     allowed_origins = ["https://www.${var.domain_name}"]
