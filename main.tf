d60360454f6ab915b7d6ded5d49bce1a9ecb2107
Khadijah <khadijah.celestine@gmail.com>
2023-04-04 12:39:18 -0400

    changed to fix build?

--- main.tf
+++ main.tf
@@ -106,14 +106,14 @@ resource "aws_api_gateway_rest_api" "resume_api_gateway" {
 }
 
 resource "aws_api_gateway_resource" "countResource" {
-  rest_api_id = data.aws_api_gateway_rest_api.resume_api_gateway.id
-  parent_id   = data.aws_api_gateway_rest_api.resume_api_gateway.root_resource_id # In this case, the parent id should the gateway root_resource_id.
+  rest_api_id = aws_api_gateway_rest_api.resume_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.resume_api_gateway.root_resource_id # In this case, the parent id should the gateway root_resource_id.
   path_part   = "count"
 }
 
 resource "aws_api_gateway_method" "postCount" {
-  rest_api_id   = data.aws_api_gateway_rest_api.resume_api_gateway.id
-  resource_id   = data.aws_api_gateway_resource.countResource.id
+  rest_api_id   = aws_api_gateway_rest_api.resume_api_gateway.id
+  resource_id   = aws_api_gateway_resource.countResource.id
   http_method   = "POST"
   authorization = "NONE"
 }
