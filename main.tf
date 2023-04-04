76edb44b3392bf22b668a436241bcfa5e802385f
Khadijah <khadijah.celestine@gmail.com>
2023-04-04 10:48:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,3 +59,46 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
     ]
   }
 }
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda_file" {
+  type        = "zip"
+  source_file = "default-lambda.py"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.9"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
