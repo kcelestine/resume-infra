89736d98cb968cc88b0dd127008f938014e5e01c
Khadijah <khadijah.celestine@gmail.com>
2023-04-04 13:06:33 -0400

    removed strings from depends_on. fix build?

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "aws_api_gateway_integration" "integration" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
-    "aws_api_gateway_integration.integration"
+    aws_api_gateway_integration.integration
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.resume_api_gateway.id}"
