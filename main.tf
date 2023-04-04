6e3083924ed69a1f63bfa6bbbaf79f0b023ec674
Khadijah <khadijah.celestine@gmail.com>
2023-04-04 11:03:32 -0400

    remove source code has, causing error
    Initializing plugins and modules...
    ╷
    │ Error: Reference to undeclared resource
    │ 
    │   on main.tf line 95, in resource "aws_lambda_function" "lambda":
    │   95:   source_code_hash = data.archive_file.lambda.output_base64sha256
    │ 
    │ A data resource "archive_file" "lambda" has not been declared in the root
    │ module.
    ╵
    Operation failed: failed running terraform plan (exit 1)

--- main.tf
+++ main.tf
@@ -92,8 +92,6 @@ resource "aws_lambda_function" "lambda" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
   runtime = "python3.9"
 
   environment {
