1509c5d7a163c36f453ae412c1685aa608478081
Khadijah <khadijah.celestine@gmail.com>
2023-04-04 13:29:07 -0400

    trying a ting

--- main.tf
+++ main.tf
@@ -127,3 +127,22 @@ resource "aws_api_gateway_integration" "integration" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
+
+resource "aws_api_gateway_integration" "lambdapy" {
+   rest_api_id = aws_api_gateway_rest_api.examplepy.id
+   resource_id = aws_api_gateway_method.number.resource_id
+   http_method = aws_api_gateway_method.number.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS"
+   uri                     = aws_lambda_function.examplepy.invoke_arn
+   passthrough_behavior = "WHEN_NO_TEMPLATES"
+   
+   request_templates = {
+    "application/json" = <<EOF
+{
+  "hour" : $input.params('hour')
+  }
+EOF
+  }
+}
