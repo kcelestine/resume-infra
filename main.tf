455b4e828597b35525a946dd77a7e93c89034d1d
Khadijah <khadijah.celestine@gmail.com>
2023-04-04 13:38:39 -0400

    trying to add deployment
    might need to add lambda role?

--- main.tf
+++ main.tf
@@ -128,14 +128,8 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
-resource "aws_api_gateway_integration" "lambdapy" {
-   rest_api_id = aws_api_gateway_rest_api.examplepy.id
-   resource_id = aws_api_gateway_method.number.resource_id
-   http_method = aws_api_gateway_method.number.http_method
-
-   integration_http_method = "POST"
-   type                    = "AWS"
-   uri                     = aws_lambda_function.examplepy.invoke_arn
-   passthrough_behavior = "WHEN_NO_TEMPLATES"
-   
+resource "aws_api_gateway_deployment" "test" {
+  depends_on  = ["aws_api_gateway_integration.test"]
+  rest_api_id = "${aws_api_gateway_rest_api.test.id}"
+  stage_name  = "dev"
 }
