9d284b65a0c03fae7290bed077d921116dfddb1e
Khadijah <khadijah.celestine@gmail.com>
2023-04-04 13:15:58 -0400

    Update api-gateway.tf

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,9 +1,4 @@
 resource "aws_api_gateway_deployment" "example" {
-  depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
-  ]
-
   rest_api_id = "${aws_api_gateway_rest_api.resume_api_gateway.id}"
   stage_name  = "prod"
 }
