574182ef3a471f2e79f7bb52772064cc2bac691c
Khadijah <khadijah.celestine@gmail.com>
2023-04-04 13:51:14 -0400

    update output, fix build?

--- main.tf
+++ main.tf
@@ -139,5 +139,5 @@ resource "aws_api_gateway_deployment" "deployment" {
 }
 
 output "base_url" {
-  value = "${aws_api_gateway_deployment.examplepy.invoke_url}"
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}"
 }
